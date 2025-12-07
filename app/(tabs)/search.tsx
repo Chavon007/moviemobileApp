d0e02a16c69152fa3a45bbc1cfb8ec94b324c828
azuh salvation <salvationazuh@gmail.com>
2025-12-07 07:46:42 +0100

    update

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,100 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
 import React from "react";
+import { Image } from "react-native";
+import MovieCard from "@/components/movieCard";
+import { useState, useEffect } from "react";
 
+import { fetchMovies } from "../../services/api";
+import logo from "../../assets/images/logo.jpeg";
+import SearchBar from "@/components/searchfile";
 const Search = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const [movies, setMovies] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const loadmovies = async () => {
+      try {
+
+        if(searchQuery.trim()){
+          await loadmovies();
+        } else{
+          reset()
+        }
+        setLoading(true);
+
+        const data = await fetchMovies({ query: searchQuery }, false);
+        setMovies(data);
+      } catch (err: any) {
+        setError(err.message || "Failed to load movies");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadmovies();
+  }, [searchQuery]);
   return (
-    <View>
-      <Text></Text>
+    <View className="flex-1 bg-[#001233]">
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => (
+          <MovieCard
+            id={item.id}
+            poster_path={item.poster_path}
+            title={item.title}
+            vote_average={item.vote_average}
+            release_date={item.release_date}
+          />
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        className="px-5"
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center">
+              <Image
+                source={logo}
+                className="w-12 h-10 flex mt-20 mb-2 mx-auto"
+              ></Image>
+            </View>
+            <View className="my-5">
+              <SearchBar
+                placeholder="Search movies..."
+                value={searchQuery}
+                onChangeText={(text:string) => setSearchQuery(text)}
+              />
+            </View>
+
+            {loading && (
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="my-3"
+              />
+            )}
+
+            {error && (
+              <Text className="text-red-500 px-5 my-3">Error: {error}</Text>
+            )}
+
+            {!loading && !error && searchQuery.trim() && movies?.length > 0 && (
+              <Text className="text-xl text-white font-bold">
+                Search result for {""}
+                <Text className="text-accent">{searchQuery}</Text>
+              </Text>
+            )}
+          </>
+        }
+      ></FlatList>
     </View>
   );
 };
--- components/movieCard.tsx
+++ components/movieCard.tsx
@@ -2,7 +2,7 @@ import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
 import { Image } from "react-native";
-import star from "../assets/icons/star.png"
+import star from "../assets/icons/star.png";
 
 interface movies {
   poster_path: string;
@@ -27,15 +27,25 @@ const MovieCard = ({
             uri: poster_path
               ? `https://image.tmdb.org/t/p/w500${poster_path}`
               : `https://placeold.co/600*400/1a1a1a/ffffff.png`,
-          }} className="w-full h-52 rounded-lg"
+          }}
+          className="w-full h-52 rounded-lg"
           resizeMode="cover"
         />
-        <Text className="text-white">{title}</Text>
+        <Text className="text-white" numberOfLines={1}>{title}</Text>
 
         <View className="flex-row  items-center justify-start gap-x-1">
-            <Image source={star} className="size-4"/>
-            <Text className="text-xs text-white">{Math.round(vote_average / 2)}</Text>
+          <Image source={star} className="size-4" />
+          <Text className="text-xs text-white">
+            {Math.round(vote_average / 2)}
+          </Text>
+        </View>
 
+        <View className="flex-row items-center">
+          <Text className="text-light-300 text-xs font-medium mt-1">
+            {release_date?.split("-")[0]}
+          </Text>
+
+         
         </View>
       </TouchableOpacity>
     </Link>
